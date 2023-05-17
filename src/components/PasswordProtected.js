92df4ecd812af0d455df254590162eb0ad5a9683
hohodsj <hohodsj@gmail.com>
2023-05-17 11:31:27 -0400

    udpate

--- src/components/PasswordProtected.js
+++ src/components/PasswordProtected.js
@@ -43,7 +43,6 @@ const PasswordProtect = () => {
         :
         <Form onSubmit={checkPw} className="mt-5 center" style={{backgroundImage: `url(${img})`, backgroundSize: `cover`}}>
              <Row className="mb-3">
-                <p>{img}</p>
                  <Form.Label>When is your birthday?</Form.Label>
                  <Form.Control type="number" id="password" required />
                  <Button type="submit">Submit</Button>
--- src/components/subcomponents/Reward.js
+++ src/components/subcomponents/Reward.js
@@ -1,19 +1,27 @@
 import React,{useEffect, useState} from "react";
-import axios from "axios";
-import Form from 'react-bootstrap/Form';
-import InputGroup from 'react-bootstrap/InputGroup';
-import Button from 'react-bootstrap/Button';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-
+import { useLocation } from "react-router-dom";
+import Confetti from "react-confetti";
+import { Image } from "react-bootstrap";
 const base_url = process.env.REACT_APP_KF_BASE_URL
 
-const RewardPage = (props) => {
+const RewardPage = ({route, navigate}) => {
     //const [question, setQuestion] = useState( () => props.question);
+    const location = useLocation();
+    //const {data} = state;
 
     return (
-        <div>
-            {props.reward}
+        <div style={{display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh'}}>
+            <Confetti width="3000" height="3000" />
+            {location.state?.url ? 
+                <div>
+                    <p className="text-secondard">Happy Birthday Sweetie!</p>
+                    <Image img src={location.state.url} /> 
+                </div>
+                : <span></span>}
+            
         </div>
     )
 }
